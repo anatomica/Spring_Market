010e541e0bc0adcb098e59b3ce619481e4c53911
Maxim <maksfomin@gmail.com>
2020-08-22 03:45:28 +0300

    Create HW 12 step 1

--- src/main/java/com/anatomica/market/MarketApplication.java
+++ src/main/java/com/anatomica/market/MarketApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MarketApplication {
 
 	// products: http://localhost:8189/market/#!/products
+	// productsWS: http://localhost:8189/market/ws
 	// swagger: http://localhost:8189/market/swagger-ui.html
 
 	public static void main(String[] args) {
