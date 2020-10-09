bd8d189bc4cf1e9681292d0116ce5fd968794cd5
Anatomica <maksfomin@gmail.com>
2020-10-09 18:44:49 +0300

    Repair db.migration sql

--- src/main/resources/db/migration/V2__categories.sql
+++ src/main/resources/db/migration/V2__categories.sql
@@ -1,5 +1,5 @@
 DROP TABLE IF EXISTS Categories CASCADE;
-CREATE TABLE Categories (id bigserial PRIMARY KEY, id_category INT, name VARCHAR(255), foreign key (id_category) references products.products (id));
+CREATE TABLE Categories (id bigserial PRIMARY KEY, id_category INT, name VARCHAR(255), foreign key (id_category) references public.products (id));
 INSERT INTO Categories
 (id_category, name) values
 (1, 'Категория 1'),
